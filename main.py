8c11e3ea98509a00244f5b3932905481f3b4a9cf
cristian-nicolae <nicolae.cristianmihai@gmail.com>
2020-12-30 19:42:21 +0200

    Added rand opponent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import pygame_gui
+import random
 
 pygame.init()
 pygame.display.set_caption('Tic Tac Toe')
@@ -53,10 +54,10 @@ button3_3 = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((200 * 2, 400
 board = [[button1_1, button1_2, button1_3], [button2_1, button2_2, button2_3], [button3_1, button3_2, button3_3]]
 
 
-# def random_computer():
-# #
-
-
+def random_computer():
+    a = free_squares()
+    r = random.randrange(0, len(a))
+    board[a[r][0]][a[r][1]].set_text('o')
 
 
 def check_win():
@@ -64,14 +65,15 @@ def check_win():
     for row in range(0, 3):
         if board[row][0].text == board[row][1].text == board[row][2].text and board[row][0].text != '':
             print("Winner is " + board[row][0].text)
-            print(board[row][0].text != '')
             break
     for col in range(0, 3):
         if board[0][col].text == board[1][col].text == board[2][col].text and board[0][col].text != '':
-            winner = board[0][col].text
             print("Winner is " + board[col][0].text)
             break
-
+    if board[0][0].text == board[1][1].text == board[2][2].text and board[0][0].text != '':
+        print("Winner is " + board[0][0].text)
+    if board[0][2].text == board[1][1].text == board[2][0].text and board[0][2].text != '':
+        print("Winner is " + board[0][2].text)
 
 
 def free_squares():
@@ -118,7 +120,7 @@ while is_running:
                     for button in board[i]:
                         if event.ui_element == button and button.text == '':
                             button.set_text("x")
-                            # draw_xo(1, 2)
+                            random_computer()
                             check_win()
                             print(free_squares())
 
